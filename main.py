7e538208becde9fa7e9a57a6cae86436701e641e
LeeKLTW <LeeKLTW@gmail.com>
2019-06-19 13:13:14 +0800

    base flag

--- main.py
+++ main.py
@@ -2,21 +2,11 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
-from absl import app
-from absl import flags
-
 import tensorflow as tf
 
-from model import data_pipeline
-from model import optimizer
-from model import transformer
-from model import translate
+from absl import app
 
-from utils import compute_bleu
-from utils import tokenizer
-from utils import misc
-from utils.logs import logger
+from utils.flags import define_transformer_flags
 
 
 class TransFormerTask(object):
@@ -25,4 +15,11 @@ class TransFormerTask(object):
 
 
 def main(_):
-  flags_obj = flags.FLAGS
+  pass
+
+
+if __name__ == '__main__':
+  tf.logging.set_verbosity(tf.logging.DEBUG)
+  define_transformer_flags()
+  app.run(main)
+
