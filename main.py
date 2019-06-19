1ee6710b491d10d1483fdbdcc006f94fdc3f8ad1
LeeKLTW <LeeKLTW@gmail.com>
2019-06-19 17:51:48 +0800

    logger context, and task skeleton

--- main.py
+++ main.py
@@ -8,21 +8,38 @@ from absl import app
 from absl import flags
 
 from utils.flags._transformer import define_transformer_flags
+from utils.logs import logger
 
 
-class TransFormerTask(object):
+class TransformerTask(object):
   def __init__(self, flags_obj):
     self.flags_obj = flags_obj
+
+  # todo
+  def train(self):
+    pass
 
 
 def main(_):
   flags_obj = flags.FLAGS
+  # todo logger
+  with logger.benchmark_context(flags_obj):
+    task = TransformerTask(flags_obj)
+    if flags_obj.mode == "train":
+      # todo class task
+      task.train()
 
-  pass
+    elif flags_obj.mode == "predict":
+      pass
+
+    elif flags_obj.mode == "eval":
+      pass
+
+    else:
+      raise ValueError("Invalid mode {}".format(flags_obj.mode))
 
 
 if __name__ == '__main__':
   tf.logging.set_verbosity(tf.logging.DEBUG)
   define_transformer_flags()
   app.run(main)
-
