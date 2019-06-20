ccb619905673b861d4e3b566c6dd5133635a2de4
LeeKLTW <LeeKLTW@gmail.com>
2019-06-20 18:09:28 +0800

    init class Transformer(tf.keras.Model)

--- model/transformer.py
+++ model/transformer.py
@@ -1,4 +1,58 @@
 # -*- coding: utf-8 -*-
+# Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Defines the Transformer model in TF 2.0.
+
+Model paper: https://arxiv.org/pdf/1706.03762.pdf
+Transformer model code source: https://github.com/tensorflow/tensor2tensor
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import tensorflow as tf
+
+from .metrics import MetricLayer
+
+class Transformer(tf.keras.Model):
+  """Transformer model with Keras.
+
+  Implemented as described in: https://arxiv.org/pdf/1706.03762.pdf
+
+  The Transformer model consists of an encoder and decoder. The input is an int
+  sequence (or a batch of sequences). The encoder produces a continuous
+  representation, and the decoder uses the encoder output to generate
+  probabilities for the output sequence.
+  """
+  def __init__(self,params,name):
+    pass
+
+  def get_config(self):
+    pass
+
+  def call(self,inputs,training):
+    pass
+
+  def encode(self):
+    pass
+
+  def decode(self):
+    pass
+
+
+
 def create_model(params, is_train):
   with tf.name_scope("model"):
     if is_train:
