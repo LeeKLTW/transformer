5871853d0f99572c4c09a26412ac8e6423a60b36
LeeKLTW <LeeKLTW@gmail.com>
2019-06-20 18:09:11 +0800

    create_model()

--- model/transformer.py
+++ model/transformer.py
@@ -1,3 +1,12 @@
 # -*- coding: utf-8 -*-
 def create_model(params, is_train):
-  pass
+  with tf.name_scope("model"):
+    if is_train:
+      inputs = tf.keras.layers.Input((None,), dtype="int64", name="inputs")
+      targets = tf.keras.layers.Input((None,), dtype="int64", name="targets")
+      internal_model = Transformer(params,name="transformer")
+      logits = internal_model([inputs, targets], training=is_train)
+      vocab_size = params["vocab_size"]
+      label_smoothing = params["label_smoothing"]
+      logits = metrics.MetricLayer(vocab_size)([logits, targets])
+
