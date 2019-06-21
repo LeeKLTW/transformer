1de64e04e4a918500f4684db535caca28188be77
LeeKLTW <LeeKLTW@gmail.com>
2019-06-21 15:56:14 +0800

    metrics

--- model/metrics.py
+++ model/metrics.py
@@ -15,7 +15,7 @@ def _pad_tensors_to_same_length(x, y):
     y_length = tf.shape(y)[1]
     max_length = tf.maximum(x_length, y_length)
 
-    x = tf.pad(x, [[0, 0], [0, max_length - x_length], [0, 0]])
+    x = tf.pad(x, [[0, 0], [0, max_length - x_length], [0, 0]])  # rank of x = 3
     y = tf.pad(y, [[0, 0], [0, max_length - y_length]])
   return x, y
 
@@ -27,6 +27,7 @@ def padded_cross_entropy_loss(logits, labels, smoothing, vocab_size):
 
     with tf.name_scope("smoothing_cross_entropy"):  # smoothing=epsilon
       confidence = 1.0 - smoothing
+
       # uniform distribution
       low_confidence = (1.0 - confidence) / tf.cast(vocab_size - 1, tf.float32)
 
@@ -57,23 +58,73 @@ def padded_accuracy(logits, labels):
     return tf.cast(tf.equal(outputs, padded_labels), tf.float32), weights
 
 
-def padded_sequence_accuracy():
-  pass
+def padded_sequence_accuracy(logits, labels):
+  """Percentage of times that predictions matches labels everywhere (non-0)."""
+  with tf.name_scope("padded_sequence_accuracy"):
+    logits, labels = _pad_tensors_to_same_length(logits, labels)
+    weights = tf.cast(tf.not_equal(labels, 0), tf.float32)
+    outputs = tf.cast(tf.argmax(logits, axis=-1), tf.int32)
+    padded_labels = tf.cast(labels, tf.int32)
+    not_correct = tf.cast(tf.not_equal(outputs, padded_labels),
+                          tf.float32) * weights
+    axis = list(range(1, len(outputs.get_shape())))
+    correct_seq = 1.0 - tf.minimum(1.0, tf.reduce_sum(not_correct, axis=axis))
+    return correct_seq, tf.constant(1.0)
 
 
-def padded_accuracy_top5():
-  pass
+def padded_accuracy_topk(logits, labels, k):
+  """Percentage of times that top-k predictions matches labels on non-0s."""
+  with tf.name_scope("padded_accuracy_topk"):
+    logits, labels = _pad_tensors_to_same_length(logits, labels)
+    weights = tf.cast(tf.not_equal(labels, 0), tf.float32)
+    effective_k = tf.minimum(k, tf.shape(logits)[-1])
+    _, outputs = tf.nn.top_k(logits, k=effective_k)
+    outputs = tf.cast(outputs, tf.int32)
+    padded_labels = tf.cast(labels, tf.int32)
+    padded_labels = tf.expand_dims(padded_labels, axis=-1)
+    padded_labels += tf.zeros_like(outputs)  # Pad to same shape.
+    same = tf.cast(tf.equal(outputs, padded_labels), tf.float32)
+    same_topk = tf.reduce_sum(same, axis=-1)
+    return same_topk, weights
+
+def padded_accuracy_top5(logits, labels):
+  return padded_accuracy_topk(logits, labels, 5)
 
 
 class MetricLayer(keras.layers.Layer):
   """Custom a layer of metrics for Transformer model."""
 
-  def __init__(self, vocab_size):
-    super(MetricLayer, self).__init__()
+  def __init__(self, vocab_size, **kwargs):
+    super(MetricLayer, self).__init__(**kwargs)
     self.vocab_size = vocab_size
-    self.metrics_fn = []
+    self.metrics_fn = [padded_cross_entropy_loss, padded_neg_log_perplexity]
 
   def build(self, input_shape):
     neg_log_perplexity = partial(padded_neg_log_perplexity,
                                  vocab_size=self.vocab_size)
-    self.metrics_fn = []
+    self.metric_mean_fns = [
+      (tf.metrics.mean, padded_accuracy, "accuracy"),
+      (tf.metrics.mean, padded_accuracy_top5, "accuracy_top5"),
+      (tf.metrics.mean, padded_sequence_accuracy, "accuracy_per_sequence"),
+      (tf.metrics.mean, neg_log_perplexity, "neg_log_perplexity"),
+    ]
+    super(MetricLayer, self).build(input_shape)
+
+  def get_config(self):
+    return {"vocab_size": self.vocab_size}
+
+  def call(self, inputs, **kwargs):
+    logits, targets = inputs[0], inputs[1]
+    for mean, fn, name in self.metric_mean_fns:
+      m = mean(*fn(logits, targets), name=name)
+      # todo: how to add metrics
+    return logits
+
+
+class LossLayer(keras.layers.Layer):
+  def __init__(self, vocab_size, label_smoothing):
+    super(LossLayer, self).__init__()
+    self.vocab_size = vocab_size
+    self.label_smoothing = label_smoothing
+
+  pass
