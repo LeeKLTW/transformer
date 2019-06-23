352078c11949ee5c7f82949ba2f99c45befd4103
LeeKLTW <LeeKLTW@gmail.com>
2019-06-23 12:27:37 +0800

    SelfAttention, Attention

--- model/attention_layer.py
+++ model/attention_layer.py
@@ -1,3 +1,35 @@
 # -*- coding: utf-8 -*-
-class SelfAttention:
-  pass
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+from tensorflow import keras
+
+
+class Attention(keras.layers.Layer):
+  def __init__(self, hidden_size, num_heads, attention_dropout, **kwargs):
+    if hidden_size % num_heads != 0:
+      raise ValueError(
+        "Hidden size ({}) must be divisible by the number of heads ({})."
+          .format(hidden_size, num_heads))
+
+    super(Attention, self).__init__(**kwargs)
+    self.hidden_size = hidden_size
+    self.num_heads = num_heads
+    self.attention_dropout = attention_dropout
+
+  def build(self, input_shape):
+    pass
+
+  # special call
+  def call(self, x, bias, training, cache=None): # pylint: disable=unused-argument
+    pass
+
+  def get_config(self):
+    pass
+
+
+class SelfAttention(Attention):
+  def call(self, x, bias, training, cache=None):
+    return super(SelfAttention, self).call(x, x, bias, training, cache)
