35c382f0e0c23cf6c444435d88bc1bd88179f850
LeeKLTW <LeeKLTW@gmail.com>
2019-06-24 18:10:08 +0800

    get_padding_bias()

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -4,6 +4,7 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
+_NEG_INF = -1e9
 
 def get_padding(x, padding_value=0): # not use in encoder ??
   """Return float tensor representing the padding values in x.
@@ -20,7 +21,25 @@ def get_padding(x, padding_value=0): # not use in encoder ??
     return tf.cast(tf.equal(x, padding_value), tf.float32)
 
 def get_padding_bias(x):
-  pass
+  """Calculate bias tensor from padding values in tensor.
+
+  Bias tensor that is added to the pre-softmax multi-headed attention logits,
+  which has shape [batch_size, num_heads, length, length]. The tensor is zero at
+  non-padding locations, and -1e9 (negative infinity) at padding locations.
+
+  Args:
+    x: int tensor with shape [batch_size, length]
+
+  Returns:
+    Attention bias tensor of shape [batch_size, 1, 1, length].
+  """
+  with tf.name_scope('attention_bias'):
+    padding = get_padding(x)
+    attention_bias = padding * _NEG_INF
+    attention_bias = tf.expand_dims(tf.expand_dims(attention_bias,axis=1),axis=1)
+  return attention_bias
+
+
 
 def get_position_encoding():
   pass
