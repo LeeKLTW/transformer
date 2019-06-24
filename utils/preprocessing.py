6d52b98b6e851fc0358dd12bb46cef509da1df74
LeeKLTW <LeeKLTW@gmail.com>
2019-06-24 16:31:47 +0800

    get_padding()

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
-def get_padding_bias(inputs):
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+def get_padding(x, padding_value=0): # not use in encoder ??
+  """Return float tensor representing the padding values in x.
+
+  Args:
+    x: int tensor with any shape
+    padding_value: int value that is the index of <PAD>
+
+  Returns:
+    float tensor with same shape as x containing values 0 or 1.
+      0 -> non-padding, 1 -> padding
+  """
+  with tf.name_scope("padding"):
+    return tf.cast(tf.equal(x, padding_value), tf.float32)
+
+def get_padding_bias(x):
   pass
-
-
-def get_padding():
-  pass
-
 
 def get_position_encoding():
   pass
