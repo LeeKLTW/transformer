cf53f1bbd1f5e96153d9ab4bf9affebefe62d007
LeeKLTW <LeeKLTW@gmail.com>
2020-01-29 23:45:29 +0800

    add native_to_unicode

--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
-def native_to_unicode(param):
-  pass
+def native_to_unicode(s):
+  """Convert string to unicode (required in Python 2)."""
+  try:               # Python 2
+    return s if isinstance(s, unicode) else s.decode("utf-8")
+  except NameError:  # Python 3
+    return s
