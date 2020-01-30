3c846c5fbe28c5f022fc23c5c055c98a6f8c20aa
LeeKLTW <LeeKLTW@gmail.com>
2020-01-30 22:10:29 +0800

    init & continue

---
+++ transformer/transformer_main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from absl import logging
+INF = int(1e9)
+BLEU_DUR = "bleu"
+_SINGLE_SAMPLE = 1
+
+if __name__ == "__main__":
+  logging.set_verbosity(logging.DEBUG)
+  #TODO: continue
