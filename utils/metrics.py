f258e14ae4232c481dbe364395fe2a2815bdfa0f
LeeKLTW <LeeKLTW@gmail.com>
2020-01-30 22:05:27 +0800

    complete def _get_ngrams_with_counter(segment, max_order):

--- utils/metrics.py
+++ utils/metrics.py
@@ -6,10 +6,26 @@ from __future__ import print_function
 import math
 import numpy as np
 
-#TODO
-def _get_ngrams_with_counter():
-  pass
+import collections
 
+def _get_ngrams_with_counter(segment, max_order):
+  """Extracts all n-grams up to a given maximum order from an input segment.
+
+  Args:
+    segment: text segment from which n-grams will be extracted.
+    max_order: maximum length in tokens of the n-grams returned by this
+        methods.
+
+  Returns:
+    The Counter containing all n-grams upto max_order in segment
+    with a count of how many times each n-gram occurred.
+  """
+  ngram_counts = collections.Counter()
+  for order in range(1, max_order+1):
+    for i in range(0, len(segment) - order +1):
+      ngram = tuple(segment[i:i+order])
+      ngram_counts[ngram]+=1
+  return ngram_counts
 
 def compute_bleu(reference_corpus, translation_corpus, max_order=4,
                  use_bp=True):
