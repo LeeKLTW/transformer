c1cc8f985d2ea39fcc1cf9db8333347371f80317
LeeKLTW <LeeKLTW@gmail.com>
2020-01-31 20:55:46 +0800

    add misc.define_transformer_flags()

--- transformer/transformer_main.py
+++ transformer/transformer_main.py
@@ -4,10 +4,15 @@ from __future__ import division
 from __future__ import print_function
 
 from absl import logging
+
+from transformer import misc
+
 INF = int(1e9)
 BLEU_DUR = "bleu"
 _SINGLE_SAMPLE = 1
 
 if __name__ == "__main__":
   logging.set_verbosity(logging.DEBUG)
+  misc.define_transformer_flags()
+
   #TODO: continue
