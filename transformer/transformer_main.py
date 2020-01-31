269845de0bfac26affd44061d20cc1f8c8ce5532
LeeKLTW <LeeKLTW@gmail.com>
2020-01-31 20:57:26 +0800

    app.run(main)

--- transformer/transformer_main.py
+++ transformer/transformer_main.py
@@ -4,6 +4,7 @@ from __future__ import division
 from __future__ import print_function
 
 from absl import logging
+from absl import app
 
 from transformer import misc
 
@@ -11,8 +12,13 @@ INF = int(1e9)
 BLEU_DUR = "bleu"
 _SINGLE_SAMPLE = 1
 
+def main(_):
+  #TODO: continue
+  pass
+
 if __name__ == "__main__":
   logging.set_verbosity(logging.DEBUG)
   misc.define_transformer_flags()
+  app.run(main)
 
-  #TODO: continue
+
