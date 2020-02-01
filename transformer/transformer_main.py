69bff01df97d9f926ddd181258615353e33f60b5
LeeKLTW <LeeKLTW@gmail.com>
2020-02-01 18:16:53 +0800

    add task

--- transformer/transformer_main.py
+++ transformer/transformer_main.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from absl import app
+from absl import flags
 from absl import logging
 
 from transformer import misc
@@ -11,6 +13,11 @@ from utils.logs import logger
 INF = int(1e9)
 BLEU_DUR = "bleu"
 _SINGLE_SAMPLE = 1
+
+#TODO
+class TransformerTask(object):
+  pass
+
 
 def main(_):
   flags_obj = flags.FLAGS
