2f6d75a457c2f7bdf2a0cf293c1576c3b9e01d36
LeeKLTW <LeeKLTW@gmail.com>
2020-02-01 19:19:28 +0800

    mv utils

---
+++ transformer/utils/flags/core.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""Public interface for flag definition.
+"""
+from utils.flags import _conventions
+
+help_wrap = _conventions.help_wrap
+
+
+def get_num_gpus(flags_obj):
+  if flags_obj.num_gpus != -1:
+    return flags_obj.num_gpus
+  from tensorflow.python.client import device_lib
+  local_device_protos = device_lib.list_local_devices()
+  return sum([1 for d in local_device_protos if d.device_type =="GPU"])
