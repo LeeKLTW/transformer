fdfa904c485382b323e05f16bd0519b5876106f1
LeeKLTW <LeeKLTW@gmail.com>
2020-02-02 20:41:41 +0800

    add define_base

--- transformer/utils/flags/core.py
+++ transformer/utils/flags/core.py
@@ -92,6 +92,75 @@ def get_loss_scale(flags_obj, default_for_fp16):
     return default_for_fp16
 
 
+def define_base(data_dir=True, model_dir=True, clean=False, train_epochs=False,
+                epochs_between_evals=False, stop_threshold=False,
+                batch_size=True, num_gpu=False, hooks=False, export_dir=False,
+                distribution_strategy=False, run_eagerly=False):
+  """Register base flags.
+
+  Args:
+    data_dir: Create a flag for specifying the input data directory.
+    model_dir: Create a flag for specifying the model file directory.
+    clean: Create a flag for removing the model_dir.
+    train_epochs: Create a flag to specify the number of training epochs.
+    epochs_between_evals: Create a flag to specify the frequency of testing.
+    stop_threshold: Create a flag to specify a threshold accuracy or other
+      eval metric which should trigger the end of training.
+    batch_size: Create a flag to specify the batch size.
+    num_gpu: Create a flag to specify the number of GPUs used.
+    hooks: Create a flag to specify hooks for logging.
+    export_dir: Create a flag to specify where a SavedModel should be exported.
+    distribution_strategy: Create a flag to specify which Distribution Strategy
+      to use.
+    run_eagerly: Create a flag to specify to run eagerly op by op.
+  Returns:
+    A list of flags for core.py to marks as key flags.
+  """
+  key_flags = []
+  if data_dir:
+    flags.DEFINE_string(name="data_dir", short_name="dd", default="/tmp",
+                        help=help_wrap("The location of the input data"))
+    key_flags.append("data_dir")
+
+  if model_dir:
+    flags.DEFINE_string(
+      name="model_dir", short_name="md", default="/tmp",
+      help=help_wrap("The location of the model checkpoint files."))
+    key_flags.append("model_dir")
+
+  if clean:
+    flags.DEFINE_boolean(
+      name="clean", default=False,
+      help=help_wrap("If set, model_dir will be removed if it exists."))
+    key_flags.append("clean")
+
+  if train_epochs:
+    flags.DEFINE_integer(
+      name="train_epochs", short_name="te", default=1,
+      help=help_wrap("The number of epochs used to train."))
+    key_flags.append("train_epochs")
+
+  if epochs_between_evals:
+    flags.DEFINE_integer(
+      name="epochs_between_evals", short_name="ebe", default=1,
+      help=help_wrap(
+        "The number of training epochs to run between evaluations."))
+    key_flags.append("epochs_between_evals")
+
+  if stop_threshold:
+    flags.DEFINE_float(
+      name="stop_threshold", short_name="st", default=None,
+      help=help_wrap("If passed, training will stop at the earlier of training_"
+                     "epochs and when the evaluation metric is greater than or "
+                     "equal to stop_threshold"))
+  if batch_size:
+    flags.DEFINE_integer(
+      name="batch_size", short_name="bs", default=32,
+      help=help_wrap("Batch size for training and evaluation. When using "
+                     "multiple GPUs, this is the global batch size for all "
+                     "devices. For example, if the batch size is 32 and there "
+                     "are 4 GPUs, each GPU will get 8 examples on each step."))
+    key_flags.append("batch_size")
 
   if num_gpu:
     flags.DEFINE_integer(
