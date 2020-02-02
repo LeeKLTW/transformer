518ee55173e5f60db83a786094f716980751c864
LeeKLTW <LeeKLTW@gmail.com>
2020-02-02 17:27:42 +0800

    complete model_params

--- transformer/model_params.py
+++ transformer/model_params.py
@@ -2,8 +2,6 @@
 """Defines Transformer model parameters."""
 from collections import defaultdict
 
-# TODO
-
 BASE_PARAMS = defaultdict(
   lambda: None,
   default_batch_size=2048,
@@ -40,11 +38,27 @@ BASE_PARAMS = defaultdict(
 )
 
 BIG_PARAMS = BASE_PARAMS.copy()
+BIG_PARAMS.update(
+  default_batch_size=4096,
+  default_batch_size_tpu=16384,
+
+  hidden_size=1024,
+  filter_size=4096,
+  num_heads=16,
+)
 
 TINY_PARAMS = BASE_PARAMS.copy()
+TINY_PARAMS.update(
+  learning_rate_warmup_steps=8000,
+)
 
 BASE_MULTI_GPU_PARAMS = BASE_PARAMS.copy()
-BASE_MULTI_GPU_PARAMS.update()
+BASE_MULTI_GPU_PARAMS.update(
+  learning_rate_warmup_steps=8000,
+)
 
 BIG_MULTI_GPU_PARAMS = BASE_PARAMS.copy()
-BIG_MULTI_GPU_PARAMS.update()
+BIG_MULTI_GPU_PARAMS.update(
+  layer_postprocess_dropout=0.3,
+  learning_rate_warmup_steps=8000,
+)
