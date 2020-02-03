e91b2f362dc58ab09ac2c2e708fe7ccd151bbead
LeeKLTW <LeeKLTW@gmail.com>
2020-02-03 08:46:44 +0800

    complete  get_logging_tensor_hook

--- transformer/utils/logs/hooks_helper.py
+++ transformer/utils/logs/hooks_helper.py
@@ -1,7 +1,66 @@
 # -*- coding: utf-8 -*-
+"""Hooks helper to return a list of TensorFlow hooks for training by name.
+
+More hooks can be added to this set. To add a new hook, 1) add the new hook to
+the registry in HOOKS, 2) add a corresponding function that parses out necessary
+parameters.
+"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-HOOKS = {}
-#TODO
+import tensorflow as tf
+
+_TENSORS_TO_LOG = dict((x, x) for x in ['learning_rate',
+                                        'cross_entropy',
+                                        'train_accuracy'])
+
+
+def get_logging_tensor_hook(every_n_iter=100, tensor_to_log=None, **kwargs):
+  """Function to get LoggingTensorHook.
+
+  Args:
+    every_n_iter: `int`, print the values of `tensors` once every N local
+      steps taken on the current worker.
+    tensors_to_log: List of tensor names or dictionary mapping labels to tensor
+      names. If not set, log _TENSORS_TO_LOG by default.
+    **kwargs: a dictionary of arguments to LoggingTensorHook.
+
+  Returns:
+    Returns a LoggingTensorHook with a standard set of tensors that will be
+    printed to stdout.
+  """
+  if tensor_to_log is None:
+    tensor_to_log = _TENSORS_TO_LOG
+
+  return tf.estimator.LoggingTensorHook(
+    tensors=tensor_to_log,
+    every_n_iter=every_n_iter)
+
+
+# TODO
+def get_profiler_hook():
+  pass
+
+
+# TODO
+def get_example_per_second_hook():
+  pass
+
+
+# TODO
+def get_logging_metric_hook():
+  pass
+
+
+# TODO
+def get_step_counter_hook():
+  pass
+
+
+HOOKS = {'loggingtensorhook': get_logging_tensor_hook,
+         'profilerhook': get_profiler_hook,
+         'examplespersecondhook': get_example_per_second_hook,
+         'loggingmetrichook': get_logging_metric_hook,
+         'stepcounterhook': get_step_counter_hook,
+         }
