6440709729cc2de2beb873f49ad2c49f094a41f3
LeeKLTW <LeeKLTW@gmail.com>
2020-02-03 08:26:09 +0800

    complete def _mirrored_cross_device_ops(all_reduce_alg, num_packs):

--- transformer/utils/misc/distribution_utils.py
+++ transformer/utils/misc/distribution_utils.py
@@ -28,7 +28,6 @@ def _collective_communication(all_reduce_alg):
   return collective_communication_options[all_reduce_alg]
 
 
-# TODO
 def _mirrored_cross_device_ops(all_reduce_alg, num_packs):
   """Return a CrossDeviceOps based on all_reduce_alg and num_packs.
 
@@ -42,6 +41,19 @@ def _mirrored_cross_device_ops(all_reduce_alg, num_packs):
   Raises:
     ValueError: if `all_reduce_alg` not in [None, 'nccl', 'hierarchical_copy'].
   """
+  if all_reduce_alg is None:
+    return None
+  mirrored_all_reduce_options = {
+    "nccl": tf.distribute.NcclAllReduce,
+    "hierarchical": tf.distribute.HierarchicalCopyAllReduce
+  }
+  if all_reduce_alg not in mirrored_all_reduce_options:
+    raise ValueError("When used with `mirrored`, valid values for all_reduce_alg"
+                     "are ['nccl', 'hierarchical_copy'], supplied value:{}"
+                     .format(all_reduce_alg))
+  cross_device_ops_class = mirrored_all_reduce_options[all_reduce_alg]
+  return cross_device_ops_class(num_packs=num_packs)
+
   pass
 
 
