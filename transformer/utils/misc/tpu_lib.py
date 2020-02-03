72a7d1af5c3141036a45beb053eda5209a4eab2d
LeeKLTW <LeeKLTW@gmail.com>
2020-02-03 08:30:07 +0800

    complete tpu_lib with def tpu_initialize(tpu_address):

--- transformer/utils/misc/tpu_lib.py
+++ transformer/utils/misc/tpu_lib.py
@@ -1,4 +1,19 @@
 # -*- coding: utf-8 -*-
-#TODO
+"""Initializes TPU system for TF 2.0."""
+import tensorflow as tf
+
 def tpu_initialize(tpu_address):
-  pass
+  """Initializes TPU for TF 2.0 training.
+
+  Args:
+    tpu_address: string, bns address of master TPU worker.
+
+  Returns:
+    A TPUClusterResolver.
+  """
+  cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver(
+    tpu=tpu_address)
+  if tpu_address not in ('', 'local'):
+    tf.config.experimental_connect_to_cluster(cluster_resolver)
+  tf.tpu.experimental.initialize_tpu_system(cluster_resolver)
+  return cluster_resolver
