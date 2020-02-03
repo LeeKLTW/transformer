b90b96daf7cceca275e838cc84050f9d99f1db1d
LeeKLTW <LeeKLTW@gmail.com>
2020-02-03 17:11:49 +0800

    undergoing define_performance()

--- transformer/utils/flags/core.py
+++ transformer/utils/flags/core.py
@@ -8,6 +8,7 @@ from __future__ import print_function
 import sys
 import codecs
 import functools
+import multiprocessing
 
 import tensorflow as tf
 from absl import app as absl_app
@@ -178,10 +179,12 @@ def define_base(data_dir=True, model_dir=True, clean=False, train_epochs=False,
       [u"\ufeee    {}".format(key) for key in hooks_helper.HOOKS]))
     flags.DEFINE_string(
       name="hooks", short_name="hk", default="LoggingTensorHook",
-      help=help_wrap(u"A list of (case insensitive) strings to specify the name "
-                     u" of training hooks. \n{}\n\ufeff Example `--hooks "
-                     u"ProfileHook, ExamplePerSecondHook`. \n "
-                     u"See transformer.utils.logs.hooks_helper for detail.".format(hook_list_str)))
+      help=help_wrap(
+        u"A list of (case insensitive) strings to specify the name "
+        u" of training hooks. \n{}\n\ufeff Example `--hooks "
+        u"ProfileHook, ExamplePerSecondHook`. \n "
+        u"See transformer.utils.logs.hooks_helper for detail.".format(
+          hook_list_str)))
     key_flags.append("hooks")
 
   if export_dir:
@@ -203,3 +206,175 @@ def define_base(data_dir=True, model_dir=True, clean=False, train_epochs=False,
                      "according to the number of GPUs."))
     key_flags.append("distribution_strategy")
   return key_flags
+
+
+# TODO
+def define_performance(num_parallel_calls=False, inter_op=False, intra_op=False,
+                       synthetic_data=False, max_train_steps=False, dtype=False,
+                       all_reduce_alg=False, num_packs=False,
+                       tf_gpu_thread_mode=False,
+                       datasets_num_private_threads=False,
+                       datasets_num_parallel_batches=False,
+                       dynamic_loss_scale=False, fp16_implementation=False,
+                       loss_scale=False,
+                       tf_data_experimental_slack=False, enable_xla=False,
+                       force_v2_in_keras_compile=False,
+                       training_dataset_cache=False):
+  """Register flags for specifying performance tuning arguments.
+
+  Args:
+    num_parallel_calls: Create a flag to specify parallelism of data loading.
+    inter_op: Create a flag to allow specification of inter op threads.
+    intra_op: Create a flag to allow specification of intra op threads.
+    synthetic_data: Create a flag to allow the use of synthetic data.
+    max_train_steps: Create a flags to allow specification of maximum number
+      of training steps
+    dtype: Create flags for specifying dtype.
+    all_reduce_alg: If set forces a specific algorithm for multi-gpu.
+    num_packs: If set provides number of packs for MirroredStrategy's cross
+      device ops.
+    tf_gpu_thread_mode: gpu_private triggers us of private thread pool.
+    datasets_num_private_threads: Number of private threads for datasets.
+    datasets_num_parallel_batches: Determines how many batches to process in
+    parallel when using map and batch from tf.data.
+    dynamic_loss_scale: Allow the "loss_scale" flag to take on the value
+      "dynamic". Only valid if `dtype` is True.
+    fp16_implementation: Create fp16_implementation flag.
+    loss_scale: Controls the loss scaling, normally for mixed-precision
+      training. Can only be turned on if dtype is also True.
+    tf_data_experimental_slack: Determines whether to enable tf.data's
+      `experimental_slack` option.
+    enable_xla: Determines if XLA (auto clustering) is turned on.
+    force_v2_in_keras_compile: Forces the use of run_distribued path even if not
+      using a `strategy`. This is not the same as
+      `tf.distribute.OneDeviceStrategy`
+    training_dataset_cache: Whether to cache the training dataset on workers.
+       Typically used to improve training performance when training data is in
+       remote storage and can fit into worker memory.
+
+  Returns:
+    A list of flags for core.py to marks as key flags.
+  """
+  key_flags = []
+  if num_parallel_calls:
+    flags.DEFINE_integer(
+      name="num_parallel_calls", short_name="npc",
+      default=multiprocessing.cpu_count(),
+      help=help_wrap("The number of records that are processed in parallel "
+                     "during input processing. This can be optimized per data "
+                     "set but for generally homogeneous data sets, should be "
+                     "approximately the number of available CPU cores."
+                     "(default behavior)"))
+
+  if inter_op:
+    flags.DEFINE_integer(
+      name="inter_op_parallelism_threads", short_name="inter", default=0,
+      help=help_wrap("Number of inter_op_parallelism_threads to use for CPU."
+                     "See TensorFlow config.protos for details."))
+
+  if intra_op:
+    flags.DEFINE_integer(
+      name="intra_op_parallelism_threads", short_name="intra", default=0,
+      help=help_wrap("Number of intra_op_parallelism_threads to use for CPU."
+                     "See TensorFlow config.protos for details."))
+
+  if synthetic_data:
+    flags.DEFINE_boolean(
+      name="use_synthetic_data", short_name="synth", default=False,
+      help=help_wrap("If set, use fake data(zeros) instead of a real dataset."
+                     "This mode is useful for performance debugging, as it "
+                     "removes input processing steps, but will not learn "
+                     "anything."))
+
+  if max_train_steps:
+    flags.DEFINE_integer(
+      name="max_train_steps", short_name="mts", default=None,
+      help=help_wrap("The model will stop training if global_step reaches this "
+                     "value. If not set, training will run until the specified "
+                     "number of epochs have run as usual. It is generally "
+                     "recommended to set --train_epochs=1 when using this flag."))
+
+  if dtype:
+    flags.DEFINE_enum(
+      name="dtype", short_name="dt", default="fp32",
+      enum_values=DTYPE_MAP.keys(),
+      help=help_wrap("The TensorFlow datatype used for calculations."
+                     "Variables may be cast to a higher precision on a "
+                     "case-by-case basis for numerical stability."))
+    loss_scale_help_text = (
+      "The amount to scale the loss by when the model is run. {}. Before "
+      "gradients are computed, the loss is multiplied by the loss scale, "
+      "making all gradients loss_scale times larger. To adjust for this, "
+      "gradients are divided by the loss scale before being applied to "
+      "variables. This is mathematically equivalent to training without a loss "
+      "scale, but the loss scale helps avoid some intermediate gradients from "
+      "underflowing to zero. If not provided default for fp16 is 128 and 1 for "
+      "other dtypes. {}"
+    )
+    if dynamic_loss_scale:
+      loss_scale_help_text = loss_scale_help_text.format(
+        "This can be an int/float or the string 'dynamic'",
+        "The string 'dynamic' can be used to dynamically determine the optimal "
+        "loss scale during training, but currently this significantly slows down "
+        "performance")
+
+      loss_scale_validation_msg = ("loss_scale should be positive int/float or "
+                                   "the string 'dynamic'.")
+    else:
+      loss_scale_help_text = loss_scale_help_text.format(
+        "This must be an int/float", "")
+      loss_scale_validation_msg = "loss_scale should be a positive int/float."
+
+  if loss_scale:
+    flags.DEFINE_string(
+      name="loss_scale:", short_name="ls",
+      default=None,
+      help=help_wrap(loss_scale_help_text))
+
+    @flags.validator(flag_name="ls", message=loss_scale_validation_msg)
+    def _check_loss_scale(loss_scale):
+      """Validator to check the loss scale flag is valid."""
+      if loss_scale is None:
+        return True
+
+      if loss_scale == "dynamic" and dynamic_loss_scale:
+        return True
+
+      try:
+        loss_scale = float(loss_scale)
+      except ValueError:
+        return False
+
+      return loss_scale > 0
+
+  if fp16_implementation:
+    flags.DEFINE_enum(
+      name="fp16_implementation", default="keras",
+      enum_values=("keras", "graph_rewrite"),
+      help=help_wrap(
+        "When --dtype=fp16, how fp16 should be implemented. This has no impact "
+        "on correctness. 'keras' use the tf.keras.mixed_precision API."
+        "'graph_rewrite' uses the "
+        "tf.train.experimental.enable_precision_graph_rewrite API."))
+
+    @flags.multi_flags_validator(flag_names=
+                                 ["fp16_implementation", "dtype", "loss_scale"])
+    def _check_fp16_implementation(flags_dict):
+      """Validator to check fp16_implementation flag is valid."""
+      if (flags_dict["fp16_implementation"] == "graph_rewrite" and
+              flags_dict["dtype"] != "fp16"):
+        raise flags.ValidationError("--fp16_implementation should not be "
+                                    "specified unless --dtype==fp16")
+      return True
+
+  pass
+
+
+# TODO
+def define_benchmark():
+  pass
+
+
+# TODO
+def define_device(tpu):
+  pass
